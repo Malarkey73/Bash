592a195212091a323771b57ddeb36da58e75fa7f
Malarkey73 <s.henderson@ucl.ac.uk>
2013-11-13 16:58:32 +0000

    some changes that may identify the Java PATH for the SGE and fastqc???-  not tested

--- fastqcCS.sh
+++ fastqcCS.sh
@@ -4,12 +4,15 @@
 #$ -l h_rt=1000:0:0
 #$ -cwd
 #$ -j y
-
+#$ -V
+  
 #shortcuts to tools
 FASTQC="/SAN/biomed/biomed13/cohesin-bio/TOOL/FastQC/FastQC/fastqc"
 # user data - note sure these are good places
 FQFOLDER="/SAN/biomed/biomed13/cohesin-bio/RNASEQ/result/20120423/FASTQ/"
 
+export JAVA_HOME="/share/apps/jdk1.7.0_45"
+export PATH=$JAVA_HOME/bin:$PATH
 
 # make sure child processes see shortcuts too
 export FASTQC; export FQFOLDER;
