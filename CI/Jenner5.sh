824dd4aee97c5b1f49e214129b7f1d0c29d3694b
Malarkey73 <s.henderson@ucl.ac.uk>
2014-04-08 11:01:54 +0100

    a number of changes including awk to make unique reads

--- CI/Jenner5.sh
+++ CI/Jenner5.sh
@@ -3,10 +3,13 @@
 # E.g.  ./Jenner.sh bamfilechip bamfileinput hg19 Hs
 # The input bam MUST BE SORTED
 
+#alternative to uniq ->  awk '!array[$2,$3, $6]++' no consecutive start, end, strand
+
 #shortcuts to tools
 UCSCtools="/home/rmgzshd/UCSCtools"
 BEDtools="/home/rmgzshd/bedtools2/bin"
 MACS="/home/rmgzshd/MACS14/bin/macs14"
+#SAMTOOLS="/home/rmgzshd/samtools/samtools"
 
 # bedtools makewindows -g hg19.chrom.sizes -w 10 > hg19.10bpbins.bed
 # this has to be done once for a genome and particular binsize
@@ -34,25 +37,25 @@ fi
 
 #ChIP sorted bam to scaled, chr filtered bed
 (
-	count_pos=$($BEDtools/bamToBed -i "$ChIP.sorted.bam" 										|
-	grep -Ew $chrN																			|
-	uniq																						|
-	$BEDtools/slopBed  -i - -g "$Genome.chrom.sizes" -l 113 -r 36 -s 							|
-	tee "$ChIP.bed" 																			|
-	wc -l           																			|
+	count_pos=$($BEDtools/bamToBed -i "$ChIP.sorted.bam" |
+	grep -Ew $chrN  |
+	awk '!array[$2,$3,$6]++' |
+	$BEDtools/slopBed  -i - -g "$Genome.chrom.sizes" -l 113 -r 36 -s |
+	tee "$ChIP.bed" |
+	wc -l |
 	awk '{print 1000000/$1}')
-	$BEDtools/genomeCoverageBed -bg  -i "$ChIP.bed" -scale $count_pos -g "$Genome.chrom.sizes" 	|
+	$BEDtools/genomeCoverageBed -bg  -i "$ChIP.bed" -scale $count_pos -g "$Genome.chrom.sizes" |
 	$BEDtools/mapBed -a $GBINS -b - -c 4 -o mean  >  "$ChIP.bedgraph"
 )&
 
 #Input sorted bam to scaled, chr filtered bed
 (
-	count_pos=$($BEDtools/bamToBed -i "$Input.sorted.bam" 										|
-	grep -Ew $chrN  																			|
-	uniq																						|
-	$BEDtools/slopBed  -i - -g "$Genome.chrom.sizes" -l 113 -r 36 -s 							|
-	tee "$Input.bed" 																			|
-	wc -l           																			|
+	count_pos=$($BEDtools/bamToBed -i "$Input.sorted.bam" |
+	grep -Ew $chrN 	|
+	awk '!array[$2,$3,$6]++' |
+	$BEDtools/slopBed  -i - -g "$Genome.chrom.sizes" -l 113 -r 36 -s |
+	tee "$Input.bed" |
+	wc -l |
 	awk '{print 1000000/$1}')
 	$BEDtools/genomeCoverageBed -bg  -i "$Input.bed" -scale $count_pos -g "$Genome.chrom.sizes" |
 	$BEDtools/mapBed -a $GBINS -b - -c 4 -o mean  >  "$Input.bedgraph"
@@ -62,22 +65,22 @@ fi
 wait
 
 #Run two MACS at same time
-$MACS -t "$Input.bed"  -c "$ChIP.bed" -g $Species --keep-dup=1 -n "$ChIP"_10-9 -p 1e-6 &
-$MACS -t "$Input.bed"  -c "$ChIP.bed" -g $Species --keep-dup=1 -n "$ChIP"_10-7 -p 1e-6 &
+#$MACS -t "$Input.bed"  -c "$ChIP.bed" -g $Species --keep-dup=1 -n "$ChIP"_10-9 -p 1e-6 &
+#$MACS -t "$Input.bed"  -c "$ChIP.bed" -g $Species --keep-dup=1 -n "$ChIP"_10-7 -p 1e-6 &
 
 # subtract Input from ChIP, where ChIP > Input subtract, 
 # otherwise skip that line, then > bedgraph && bigwig
-paste "$ChIP.bedgraph" "$Input.bedgraph" | awk '{if ($8 != "." && $4 > $8) {print $1,$2,$3,$4-$8}}'> "$ChIP$Input.bedgraph" &&
+paste "$ChIP.bedgraph" "$Input.bedgraph" | awk '{if ($8 != "." && $4 > $8) {print $1,$2,$3,$4-$8}else{print $1,$2,$3,0}}' | sed -e 's/ /\t/g' > "$ChIP$Input.bedgraph" &&
 $UCSCtools/bedGraphToBigWig "$ChIP$Input.bedgraph" "$Genome.chrom.sizes" "$ChIP.bw"
 
 wait
 
 # tidy up
-rm "$ChIP.bed"
-rm "$Input.bed"
-rm "$ChIP$Input.bedgraph"
-rm "$ChIP.bedgraph"
-rm "$Input.bedgraph"
+#rm "$ChIP.bed"
+#rm "$Input.bed"
+#rm "$ChIP$Input.bedgraph"
+#rm "$ChIP.bedgraph"
+#rm "$Input.bedgraph"
 
 # print time
 ENDTIME=$(date +%s)
