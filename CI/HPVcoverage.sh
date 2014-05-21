3f87cd4d24d2ca6abfb56b2a27b338377ea6640a
Malarkey73 <s.henderson@ucl.ac.uk>
2014-05-21 13:34:18 +0100

    there is abug with conversecs function in HPVcoverage.sh I've commented out till fixed

--- CI/HPVcoverage.sh
+++ CI/HPVcoverage.sh
@@ -7,7 +7,7 @@ BT2="/home/rmgzshd/bowtie2-2.1.0/bowtie2"
 BT2GENOME="/mnt/store1/cghub/HPV_BT2/HPV"
 SAMBAMBA="/home/rmgzshd/sambamba/sambamba"
 BEDTOOLS="/home/rmgzshd/bedtools2/bin/bedtools"
-GENOMESIZES="/mnt/store1/cghub/HPV_BT2/HPV.sizes"
+GENOME="/mnt/store1/cghub/HPV_BT2/HPV.sizes"
 
 export BT2; export BT2GENOME; export SAMBAMBA; export BEDTOOLS;
 
@@ -15,75 +15,59 @@ convertsecs() {
  ((h=${1}/3600))
  ((m=(${1}%3600)/60))
  ((s=${1}%60))
- printf "%02d:%02d:%02d\n" $h $m $s
+ printf "\t%02d:%02d:%02d\n" $h $m $s
 }
-
-# start timer for script
 STARTTIME=$(date +%s)
-
-
 
 for BAM in *.sorted.bam
 do
 	
 	PREFIX=$(echo ${BAM} | sed 's/.sorted.bam//')
-	printf "\n\n\t beginning %s \n " $PREFIX
+	printf "\n\n beginning %s sample.\n " $PREFIX
 	
-	$SAMBAMBA view --format=bam -F  "paired and (unmapped or mate_is_unmapped)" $BAM |
-	tee >($BEDTOOLS bamtofastq -i stdin -fq R1.fq -fq2 R2.fq)			|
-	$BEDTOOLS bamtobed -i stdin 										|
-	# I clip the mate pair char so I can easily compare to HPV bed  
-	awk '{print $1,$2,$3,substr($4, 0, length($4)-2),$5, $6}' 			> 
-	$PREFIX.anchor.bed
-	# real	3m56.793s on 20GB
-	# user	17m21.587s
-	# sys	0m39.095s
-
+	$SAMBAMBA view --format=bam -F  "paired and (unmapped or mate_is_unmapped)" $BAM 				|
+	tee >($BEDTOOLS bamtofastq -i stdin -fq R1.fq -fq2 R2.fq)										|
+	$BEDTOOLS bamtobed -bedpe -i stdin 	| sort -S8G -k 7											|
+	# I clip the mate pair ID last char read flag so I can merge with HPV bed  
+	awk '{if ($2 != -1 || $5 != -1) print $1,$2,$3,$4,$5,$6, substr($7, 0, length($7)-2), $8, $9, $10}' > $PREFIX.anchor.bedpe
+	
 	# print time
-	ENDTIME=$(date +%s)
-	printf '\n\t Finished extracting unmapped reads in: '
-	echo convertsecs $(($ENDTIME - $STARTTIME))
-	NEXTTIME==$(date +%s)
-
-	
+	NEXTTIME1=$(date +%s)
+#	printf "\n Finished extracting unmapped reads in: \n"
+#	convertsecs $(($NEXTTIME1 - $STARTTIME))
+		
 	# Use bowtie2 to find matches amongst the unmapped genome in HPV 	
-	$BT2 -p 24 -x $BT2GENOME -1 R1.fq -2 R2.fq 							| 
-	$SAMBAMBA view -S --format=bam /dev/stdin 							|
-    $BEDTOOLS bamtobed -i stdin  										|
-    # NB same clip as ANCHOR bed above so can compare mate pairs
-	awk '{print $1,$2,$3,substr($4, 0, length($4)-2),$5, $6}' 			> 
-    $PREFIX.hpv.bed
+	$BT2 -p 24 -x $BT2GENOME -1 R1.fq -2 R2.fq 				| 
+	$SAMBAMBA view -S --format=bam /dev/stdin 				|
+	tee $PREFIX.hpv.bam 							|
+    	$BEDTOOLS bamtobed -bedpe -i stdin | sort -S8G -k 7 			|
+	awk '{if ($2 != -1 || $5 != -1) print $1,$2,$3,$4,$5, $6, substr($7, 0, length($7)-2), $8, $9, $10}' > $PREFIX.hpv.bedpe
 
-    # print time
-	ENDTIME=$(date +%s)
-	printf '\n\t Finished aligning to HPV genome in: '
-	echo convertsecs $(($ENDTIME - $NEXTTIME))
-	NEXTTIME==$(date +%s)
+    	# print time
+	NEXTTIME2=$(date +%s)
+#	printf "\n Finished aligning to HPV genome in: \n"
+#	convertsecs $(($NEXTTIME2 - $NEXTTIME1))
     
-    # calculate coverage
-    $BEDTOOLS genomecov -i $PREFIX.hpv.bed -g $GENOME -bg 
-	awk '{sum+=$4} END { print sum/NR}'
-	# mean depth 138.822 on 20GB HPV16+ HNSC file TCGA-BA-5153
-
+    	# calculate coverage NB NEED TO WRITE/APPEND THIS TO A LOG FILE
+    	printf "\n HPV coverage :"
+    	$BEDTOOLS genomecov -bg -ibam $PREFIX.hpv.bam  -g $GENOME  |
+    	awk '{sum+=  qq$4} END { print sum/NR}'
+	
 	# print time
-	ENDTIME=$(date +%s)
-	printf '\n\t Finished calculating HPV coverage in: '
-	echo convertsecs $(($ENDTIME - $NEXTTIME))
-	NEXTTIME==$(date +%s)
+	NEXTTIME3=$(date +%s)
+#	printf "\n Finished calculating HPV coverage in: \n"
+#	convertsecs $(($NEXTTIME3 - $NEXTTIME2))
 
+	#awk '{if ($2!= -1 || $5 != -1)  print}'
+	#join -1 4 -2 4 sort.hpv.bed sort.anchor.bed > $PREFIX.join.bed
 
-	printf '\n\n\t Next file: '
+	printf "\n Next file?: "
 done
+printf "\n No, all done: "
 
 rm R1.fq
 rm R2.fq
-
 # print time
 ENDTIME=$(date +%s)
-printf '\n\t Finished all work in: '
-echo convertsecs $(($ENDTIME - $STARTTIME))
-
-
-
-
-
+#printf "\n\t Finished all work in: "
+#convertsecs $(($ENDTIME - $STARTTIME))
