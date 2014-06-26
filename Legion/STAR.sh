e770445dd41b53ed2c5fb50ef9a189d847cc5f24
Malarkey73 <s.henderson@ucl.ac.uk>
2014-06-26 14:56:19 +0100

    After much strife these versions of STAR scripts are working (slowly)

--- Legion/STAR.sh
+++ Legion/STAR.sh
@@ -1,31 +1,53 @@
-
-#!/bin/bash -1
-#$ -S /bin/bash 
+#!/bin/bash -l
+#$ -S /bin/bash
 
 # 2. Request ten hours of wallclock time (format hours:minutes:seconds).
 #$ -l h_rt=10:00:0
 
 # 3. Request 1 gigabyte of RAM 
-#$ -l mem=8G
+#$ -l mem=32G
 
 # 4. Request 15 gigabyte of TMPDIR space (default is 10 GB)
 #$ -l tmpfs=32G
 
 # 5. Set the name of the job.
-#$ -N HPVcoverage
+#$ -N STARalignment
 
 # 6. Find <your_project_id> by running the command "groups"
 #$ -P TCGAHPVHNSC
 
 # 7. Replace "<your_UCL_id>" with your UCL user ID :)
-#$ -wd /home/rmgzshd/Scratch/HNSC
+#$ -wd /home/rmgzshd/Scratch/Pablo
+
+# 8. request 16 threads
+#$ -l thr=16
 
 set -o nounset
 set -o errexit
 set -o pipefail
 
-#module unload compilers/intel/11.1/072
-#module load compilers/gnu/4.6.3
-#module load bowtie2
-#module load samtools
-#module load bedtools
+ENSEMBL75="/home/rmgzshd/Scratch/Genome/ENSEMBL.homo_sapiens.release-75"
+FQDATA="/home/rmgzshd/Scratch/Pablo"
+SEQTK="/home/rmgzshd/Tools/seqtk/seqtk"
+module load star/2.3.0e
+module load samtools/0.1.19
+export ENSEMBL75; export FQDATA; export SEQTK;
+#fq1=$(echo ls *R1_001.fastq.gz | sed 's/ /,/g')
+#fq2=$(echo ls *R2_001.fastq.gz | sed 's/ /,/g')
+
+
+for fq1 in *_R1_001.fastq.gz
+do
+    prefix=$(echo ${fq1} | sed 's/_R1_001.fastq.gz//')
+    fq2=${prefix}_R2_001.fastq.gz
+
+    STAR --genomeDir $ENSEMBL75 \
+	--readFilesIn $fq1 $fq2 \
+	--runThreadN 16 \
+	--readFilesCommand $SEQTK trimfq \
+	--genomeLoad NoSharedMemory \
+	--outSAMstrandField intronMotif \
+	--outStd SAM \
+	--outSAMattributes Standard | samtools view -buS - | samtools sort -m16G - $prefix
+
+done
