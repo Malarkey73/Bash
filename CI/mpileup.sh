8bd125a36cb0b2281bd243b673d03b5192d4db2f
Malarkey73 <s.henderson@ucl.ac.uk>
2015-01-27 13:07:50 +0000

    a new mpileup shellscript based on Gerolds guidelines

---
+++ CI/mpileup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+#shortcuts to tools
+SAMTOOLS="/home/rmgzshd/samtools/samtools"
+
+# shortcuts to annotation
+GENOME="/mnt/store1/GATK_BUNDLE_2.8_hg19/ucsc.hg19.fasta"
+CAPTURE_REGION="/mnt/store1/WXS_CAPTURE_BEDS/SeqCap_EZ_Exome_v3_primary.bed"
+$DATAFOLDER="/mnt/store1/CESC_WXS"
+
+hostname
+date
+$SAMTOOLS mpileup -A -B -C 50 -d 10000 -m 3 -F 0.0002 -q 20 -Q 20 \
+-f $GENOME \
+-l $CAPTURE_REGION \
+$DATAFOLDER/191e1f11c562128d1f2afb31f50e73b5.bam \
+$DATAFOLDER/2308c254ed02bbab73f9478cc398e42b.bam > \
+$DATAFOLDER/TCGA-2W-A8YY.mpileup
+
