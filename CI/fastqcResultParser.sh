5871ac3469e16b0571b40ad31ffdb8a5e40caa32
Malarkey73 <s.henderson@ucl.ac.uk>
2015-02-25 12:54:40 +0000

    finished script for parsing fastqc results in mutiple files into a single table

--- CI/fastqcResultParser.sh
+++ CI/fastqcResultParser.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-touch fastq.results
-echo "BasicStatistics/tPerBaseSequenceQuality/tPerSequenceQualityScores"
-
+# summary files and paths
 SUMMARYFILES=`find . -print | grep -i 'summary.txt'`
+FIRST=true
+
 for SUMMARY in $SUMMARYFILES
 do
-	cat $SUMMARY | 
+  	#if this is the first sample then put a header row in first
+        if [[ $FIRST = true ]]; then
+                printf "SAMPLE\t" > fastqc.results
+                cut -f 2 $SUMMARY | tr "\n" "\t" >> fastqc.results
+                printf "\n"  >>fastqc.results
+                FIRST=false
+        fi
+
+	cut -f 3 $SUMMARY | head -1 | tr "\n" "\t" >> fastqc.results
+        cut -f 1 $SUMMARY | tr "\n" "\t" >> fastqc.results
+        printf "\n" >> fastqc.results
 done
