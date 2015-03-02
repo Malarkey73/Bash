82b75609a9162d58b24399c1886d72ac73657837
Malarkey73 <s.henderson@ucl.ac.uk>
2015-03-02 14:08:17 +0000

    the fastqcReporter script works roughly as intended

--- CI/fastqcResultReporter.sh
+++ CI/fastqcResultReporter.sh
@@ -6,10 +6,12 @@
 # which is for BAM file stats.
 
 FQCFILES=`find . -print | grep -i 'fastqc_data.txt'`
+
 FIRST=true
 
 for FQC in $FQCFILES
 do
+
 	# Samplename
 	grep -n "Filename" $FQC | cut -f 2 | tr "\n" "\t" >> fastqc.data
 	#Total Sequences
@@ -18,6 +20,7 @@ do
 	grep -n -m 1 "%GC" $FQC | cut -f 2 | tr "\n" "\t" >> fastqc.data
 	printf "\n"  >>fastqc.data
 
+	
 	if [[ $FIRST = true ]]; then
 		sed -n '/>>Per base sequence quality\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2 > pb.seq.quality
 		
@@ -31,33 +34,42 @@ do
 
 		sed -n '/>>Kmer Content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2,3 > kmer.lev
 
-		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2 > tempfile
+		# checked
+		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2 | tail -n +2 > tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,3 | tail -n +2 >> tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,4 | tail -n +2 >> tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,5 | tail -n +2 >> tempfile
 		cat tempfile > pb.sequence.scores
 
 		FIRST=false
-	else 
-		paste pb.meanseq.quality <(sed -n '/>>Per base sequence quality\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2) > tempfile
+	else
+
+		#checked 
+		paste pb.seq.quality <(sed -n '/>>Per base sequence quality\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2) > tempfile
 		cat tempfile > pb.seq.quality
 		
+		#checked
 		paste ps.quality.scores <(sed -n '/>>Per sequence quality scores\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2) > tempfile
 		cat tempfile > ps.quality.scores
 		
+		#checked
 		paste pb.seq.GC <(sed -n '/>>Per base GC content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2) > tempfile
 		cat tempfile > pb.seq.GC
-		
+
+		#checked
 		paste pb.seq.N <(sed -n '/>>Per base N content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2) > tempfile
 		cat tempfile > pb.seq.N
 
+		#checked
 		paste dup.seq.lev <(sed -n '/>>Sequence Duplication Levels\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2 | tail -n +2) > tempfile
 		cat tempfile > dup.seq.lev
 
+		# still needs rejiggled
 		paste kmer.lev <(sed -n '/>>Kmer Content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2,3) > tempfile
 		cat tempfile > kmer.lev
 
-		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2 > tempfile
+		# checked
+		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2 | tail -n +2 > tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 3 | tail -n +2 >> tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 4 | tail -n +2 >> tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 5 | tail -n +2 >> tempfile
@@ -66,3 +78,6 @@ do
 
 	fi
 done
+
+rm tempfile
+rm tempfile2
