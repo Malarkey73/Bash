9afad06e9dc180edbc5a76ecf7e82f2a598ede99
Malarkey73 <s.henderson@ucl.ac.uk>
2015-03-04 14:44:41 +0000

    a working version of fastqcResultreporter with kmer levels workign now

--- CI/fastqcResultReporter.sh
+++ CI/fastqcResultReporter.sh
@@ -6,22 +6,23 @@
 # which is for BAM file stats.
 
 FQCFILES=`find . -print | grep -i 'fastqc_data.txt'`
-
 FIRST=true
 
 for FQC in $FQCFILES
 do
 
-	# Samplename
-	grep -n "Filename" $FQC | cut -f 2 | tr "\n" "\t" >> fastqc.data
-	#Total Sequences
-	grep -n "Total Sequences" $FQC | cut -f 2 | tr "\n" "\t" >> fastqc.data
-	# GC percent
-	grep -n -m 1 "%GC" $FQC | cut -f 2 | tr "\n" "\t" >> fastqc.data
-	printf "\n"  >>fastqc.data
-
+	
 	
 	if [[ $FIRST = true ]]; then
+		
+		# Samplename
+		grep -n "Filename" $FQC | cut -f 2 | tr "\n" "\t" > fastqc.data
+		#Total Sequences
+		grep -n "Total Sequences" $FQC | cut -f 2 | tr "\n" "\t" >> fastqc.data
+		# GC percent
+		grep -n -m 1 "%GC" $FQC | cut -f 2 | sed -e 's/\n//' >> fastqc.data
+		printf "\n"  >fastqc.data
+
 		sed -n '/>>Per base sequence quality\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2 > pb.seq.quality
 		
 		sed -n '/>>Per sequence quality scores\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2 > ps.quality.scores
@@ -32,17 +33,24 @@ do
 
 		sed -n '/>>Sequence Duplication Levels\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2 | tail -n +2 > dup.seq.lev
 
-		sed -n '/>>Kmer Content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2,3 > kmer.lev
+		sed -n '/>>Kmer Content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2,3 | sed -e "s|$|\t${FQC}|" | tail -n +2 > kmer.lev
 
 		# checked
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2 | tail -n +2 > tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,3 | tail -n +2 >> tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,4 | tail -n +2 >> tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,5 | tail -n +2 >> tempfile
-		cat tempfile > pb.sequence.scores
+		cat tempfile > pb.seq.GATC
 
 		FIRST=false
 	else
+		# Samplename
+		grep -n "Filename" $FQC | cut -f 2 | tr "\n" "\t" >> fastqc.data
+		#Total Sequences
+		grep -n "Total Sequences" $FQC | cut -f 2 | tr "\n" "\t" >> fastqc.data
+		# GC percent
+		grep -n -m 1 "%GC" $FQC | cut -f 2 | sed -e 's/\n//' >> fastqc.data
+		printf "\n"  >>fastqc.data
 
 		#checked 
 		paste pb.seq.quality <(sed -n '/>>Per base sequence quality\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2) > tempfile
@@ -64,9 +72,8 @@ do
 		paste dup.seq.lev <(sed -n '/>>Sequence Duplication Levels\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2 | tail -n +2) > tempfile
 		cat tempfile > dup.seq.lev
 
-		# still needs rejiggled
-		paste kmer.lev <(sed -n '/>>Kmer Content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2,3) > tempfile
-		cat tempfile > kmer.lev
+		# rejiggled and fixed!
+		sed -n '/>>Kmer Content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 1,2,3 | sed -e "s|$|\t${FQC}|" | tail -n +2 >> kmer.lev
 
 		# checked
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 2 | tail -n +2 > tempfile
@@ -74,7 +81,7 @@ do
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 4 | tail -n +2 >> tempfile
 		sed -n '/>>Per base sequence content\t/{:a;n;/>>END_MODULE/b;p;ba}' $FQC | cut -f 5 | tail -n +2 >> tempfile
 		paste pb.sequence.scores tempfile > tempfile2
-		cat tempfile2 > pb.sequence.scores
+		cat tempfile2 > pb.seq.GATC
 
 	fi
 done
